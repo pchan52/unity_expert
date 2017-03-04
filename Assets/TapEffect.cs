3d07bfb3b22b8fbc15b4ee7c6f0efe3d72ab5c9c
HideakiTouhara <hideee6723@gmail.com>
2017-03-04 18:02:02 +0900

    Edit TapEffect.cs to add tap function

--- Assets/TapEffect.cs
+++ Assets/TapEffect.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class TapEffect : MonoBehaviour {
 
+	[SerializeField] ParticleSystem _particleSystem;
+	[SerializeField] Camera _camera;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +14,11 @@ public class TapEffect : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if(Input.GetMouseButtonDown(0)) {
+			var pos = _camera.ScreenToWorldPoint(Input.mousePosition + _camera.transform.forward * 10);
+			_particleSystem.transform.position = pos;
+			_particleSystem.Emit(1);
+		}
 		
 	}
 }
