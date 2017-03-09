ffdc4629154f5675b1c8546c0863b7b3879a3dcd
池田光 <Hikaru@pchan.local>
2017-03-09 21:34:29 +0900

    Add GenericTest.cs and Mystack.cs

--- Assets/4_Script/602/GenericTest.cs
+++ Assets/4_Script/602/GenericTest.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using System;
 
 public class GenericTest : MonoBehaviour {
@@ -11,12 +9,28 @@ public class GenericTest : MonoBehaviour {
 		var answer_int = Max<int>(3,5); 
 		print (answer_int);
 
-		var answer_double = Max<double> (3.5d, 6.5d);
-		print (answer_double);
+		var answer_float = Max<float> (3.5f, 6.5f);
+		print (answer_float);
+
+		var mystack1 = new Mystack<int> (5);
+		mystack1.Push (1);
+		mystack1.Push (2);
+		mystack1.Push (3);
+		mystack1.ShowStack ();
+		mystack1.Pop ();
+		mystack1.ShowStack ();
+
+	    var mystack2 = new Mystack<float>(5);
+	    mystack2.Push(1.2f);
+	    mystack2.Push(2.4f);
+	    mystack2.Push(3.6f);
+	    mystack2.ShowStack();
+	    mystack2.Pop();
+	    mystack2.ShowStack();
 	}
 
-		
-	int Max(int x, int y){
+
+	private int Max(int x, int y){
 		if(x > y){
 			return x;
 		}else{
@@ -27,7 +41,7 @@ public class GenericTest : MonoBehaviour {
 	private T Max<T>(T x , T y)
 		where T : IComparable
 	{
-		if (x.CompareTo(y) > 0) {
+		if (x != null && x.CompareTo(y) > 0) {
 			return x;
 		} else {
 			return y;
---
+++ Assets/4_Script/602/Mystack.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Mystack<T> {
+     private int _top;
+     private readonly T[] _mystack;
+
+     public Mystack(int max)
+     {
+         _mystack = new T[max];
+         this._top = 0;
+     }
+
+     public void Push(T x){
+         this._mystack[this._top++] = x;
+     }
+
+     public T Pop(){
+         return this._mystack[--this._top];
+     }
+
+     public void ShowStack(){
+         for (var i = 0; i < _top; i++) {
+             Debug.Log (_mystack [i]);
+         }
+     }
+ }
+
