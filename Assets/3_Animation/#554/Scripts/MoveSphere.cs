02f8c098b9735fce88c82be84994905ab7dde0df
Takuma Saito <nitol5524@gmail.com>
2017-03-10 14:57:37 +0900

    Animation

---
+++ Assets/3_Animation/#554/Scripts/MoveSphere.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MoveSphere : MonoBehaviour {
+
+	// Use this for initialization
+    void Start()
+    {
+     //   transform.DOLocalPath(new Vector3[] {new Vector3(3, 3), Vector3.zero, new Vector3(-3, 2)}, 3f, PathType.CatmullRom);
+
+       //transform.DOPunchPosition(new Vector3(-5, 0), 2f);
+
+        transform.DOLocalPath (new Vector3[]{ new Vector3 (3f, 3f), Vector3.zero, new Vector3 (-3f, 2f) }, 2f, PathType.CatmullRom);
+    }
+
+    // Update is called once per frame
+	void Update ()
+	{
+
+	  //  transform.DOLocalMoveX(-3f, 2f);
+
+	}
+
+}
