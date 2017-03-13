4b2acb5072f2bd5287cfe64b2c79537305cf1b81
池田光 <Hikaru@pchan.local>
2017-03-13 16:48:03 +0900

    Done Singleton pattern

---
+++ Assets/4_Script/639/Singleton/SingletonSample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class SingletonSample {
+
+	private static SingletonSample _singleton = new SingletonSample();
+
+    private SingletonSample()
+    {
+
+        Debug.Log("Create SingletonSample instance.");
+
+    }
+
+    public static SingletonSample GetInstance
+    {
+        get{
+            if (_singleton == null) _singleton = new SingletonSample();
+
+            return _singleton;
+        }
+    }
+
+    public int testNum = 10;
+
+    public void setNum(int num)
+    {
+        testNum = num;
+    }
+}
