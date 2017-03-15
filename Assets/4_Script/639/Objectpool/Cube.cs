d5fb8019f15c1cc78fff5fa2f4d7d2db68e74e8d
池田光 <Hikaru@pchan.local>
2017-03-15 21:46:05 +0900

    done Objectpool pattern

---
+++ Assets/4_Script/639/Objectpool/Cube.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public float _activeTime = 0;
+	
+	// Update is called once per frame
+	void Update () {
+	    if (gameObject.activeInHierarchy)
+	    {
+	        _activeTime += Time.deltaTime;
+	    }
+
+	    if (_activeTime > 1.0f)
+	    {
+	        gameObject.SetActive(false);
+	        _activeTime = 0;
+	    }
+	}
+}
