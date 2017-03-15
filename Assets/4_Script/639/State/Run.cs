7670d17c3fa42f681caa1eec8de3208000d4b040
池田光 <Hikaru@pchan.local>
2017-03-15 21:46:40 +0900

    done State pattern

---
+++ Assets/4_Script/639/State/Run.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Run : IState
+{
+
+    private readonly float _speed;
+
+    public Run(float speed)
+    {
+        _speed = speed;
+    }
+
+    public void Move()
+    {
+        Debug.Log("speedA = " + _speed + "で走ってます" );
+    }
+}
