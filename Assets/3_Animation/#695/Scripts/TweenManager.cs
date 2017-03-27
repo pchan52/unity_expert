10eb0652b3326a436d03155d47ba2bca31ea35c3
Takuma Saito <nitol5524@gmail.com>
2017-03-27 11:10:30 +0900

    Create Kasumi Slider

---
+++ Assets/3_Animation/#695/Scripts/TweenManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Mime;
+using DG.Tweening;
+using UnityEngine.UI;
+public class TweenManager : MonoBehaviour
+{
+
+    [SerializeField] private Image image;
+    private RectTransform rectTransform;
+
+    private void Start () {
+        rectTransform = image.GetComponent<RectTransform>();
+        rectTransform.DOMove(new Vector3(Screen.width,0,0),5.0f);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rectTransform.DOMove(new Vector3(-1 * Screen.width,0,0),5.0f);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            rectTransform.DOMove(new Vector3(Screen.width,0,0),5.0f);
+        }
+    }
+}
