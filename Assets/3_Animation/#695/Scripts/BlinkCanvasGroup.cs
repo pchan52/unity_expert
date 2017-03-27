2362cfb9e0a5b1af40c7b931979f7ccec61f7d64
Takuma Saito <nitol5524@gmail.com>
2017-03-27 15:44:34 +0900

    Create blinking animation

---
+++ Assets/3_Animation/#695/Scripts/BlinkCanvasGroup.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class BlinkCanvasGroup : MonoBehaviour
+{
+    [SerializeField] float DurationSeconds;
+    [SerializeField] Ease EaseType;
+
+    private CanvasGroup canvasGroup;
+
+    private void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        canvasGroup.DOFade(0.0f, DurationSeconds).SetEase(EaseType).SetLoops(-1, LoopType.Yoyo);
+    }
+}
