0987501e7a5c671969d3e73bcbcfe249dd19705d
Takuma Saito <nitol5524@gmail.com>
2017-03-27 11:21:05 +0900

    Create Bounce Text

---
+++ Assets/3_Animation/#695/Scripts/BoundText.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class BoundText : MonoBehaviour {
+
+    private void Update () {
+        if (!Input.GetMouseButtonDown(0)) return;
+        // 跳ねるっぽく
+        transform.DOLocalMoveY(-30f, 2f).SetEase(Ease.OutBounce);
+        transform.DOLocalMoveX(30f, 2f);
+    }
+}
