29b448af7c34a0c7b77f532f9fbca2ab29a391ad
池田光 <Hikaru@pchan.local>
2017-03-28 22:42:13 +0900

    Add 697 folder and code

---
+++ Assets/4_Script/697/Jugarray.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugarray : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int[][] jug = {
+			new[] { 1 },
+			new[] { 2, 3, 4 }
+		};	
+		for (var i = 0; i < jug.Length; i++) {
+			print (jug [i].Length);
+			for (var j = 0; j < jug [i].Length; j++) {
+				print (jug [i] [j]);
+			}
+		}
+	}
+}
