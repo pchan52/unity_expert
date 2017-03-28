d5b8f74936e8137d57b2265782e7c1680ebafb70
池田光 <Hikaru@pchan.local>
2017-03-28 12:05:08 +0900

    Add 643 folder and code

---
+++ Assets/4_Script/643/CastTest.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CastTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    int a ;
+	    float b = 10.5f;
+	    a = (int) b;
+	    print(a);
+	}
+
+}
---
+++ Assets/4_Script/643/CastTest1.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CastTest1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    int a;
+	    string b = "23.5";
+	    a = int.Parse(b);
+	    print(a);
+	}
+
+}
+
