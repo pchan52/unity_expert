0794d2c99c110ee54a62b5bc841a76e0a854c0a5
池田光 <Hikaru@pchan.local>
2017-04-04 23:56:12 +0900

    Add 686 folder and code

---
+++ Assets/4_Script/686/Button3.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Button3 : MonoBehaviour, IButton {
+
+    public void Press()
+    {
+        Debug.Log("pressed button3");
+    }
+}
---
+++ Assets/4_Script/686/SRPModify.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class SRPModify : MonoBehaviour {
+
+    private void Update()
+    {
+        if (!Input.GetMouseButtonDown(0))
+        {
+            return;
+        }
+
+        RaycastHit hit;
+        if (!Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+        {
+            return;
+        }
+
+        var button = hit.collider.GetComponent<IButton>();
+        if (button != null)
+        {
+            button.Press();
+        }
+    }
+
+}
